d986e9c7e08c616ee8f78893c4bb205c58bbced5
OnoprienkoArtem <o.artem.e@gmail.com>
2019-10-12 23:53:18 +0300

    work with offer part

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+
+import { PlacesService } from '../../places.service';
+import { Place } from '../../place.model';
 
 @Component({
   selector: 'app-edit-offer',
@@ -7,9 +12,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditOfferPage implements OnInit {
 
-  constructor() { }
+  offerPlaces: Place;
+
+  constructor(private route: ActivatedRoute, private navController: NavController, private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this.navController.navigateBack('/places/tabs/offers');
+        return;
+      }
+
+      this.offerPlaces = this.placesService.getPlace(paramMap.get('placeId'));
+    });
   }
 
 }
