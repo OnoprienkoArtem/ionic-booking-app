ee232e93ede63d78f4a1868d4e1579645edcbb4e
OnoprienkoArtem <o.artem.e@gmail.com>
2019-10-18 20:39:42 +0300

    change angular version

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,9 +7,15 @@ export class AuthService {
 
   // tslint:disable-next-line: variable-name
   private _userIsAuthenticated = false;
+  // tslint:disable-next-line: variable-name
+  private _userId = 'abc';
 
   get userIsAuthenticated() {
     return this._userIsAuthenticated;
+  }
+
+  get userId() {
+    return this._userId;
   }
 
   constructor() { }
