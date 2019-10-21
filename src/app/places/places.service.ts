51aee8adca0cc7b3d080ffafb55523b91560be94
OnoprienkoArtem <o.artem.e@gmail.com>
2019-10-21 18:52:58 +0300

    fetch places from firebase

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -6,43 +6,56 @@ import { take, map, tap, delay, switchMap } from 'rxjs/operators';
 import { Place } from './place.model';
 import { AuthService } from '../auth/auth.service';
 
+interface PlaceData {
+    availableFrom: string;
+    availableTo: string;
+    description: string;
+    imageUrl: string;
+    price: number;
+    title: string;
+    userId: string;
+}
+
+
+// [
+//     new Place(
+//         'p1',
+//         'Manhattan Mansion',
+//         'In the heart of New York City.',
+//         'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
+//         149.99,
+//         new Date('2019-01-01'),
+//         new Date('2019-12-31'),
+//         'abc'
+//     ),
+//     new Place(
+//         'p2',
+//         "L'Amour Toujours",
+//         'A romantic place in Paris!',
+//         'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
+//         189.99,
+//         new Date('2019-01-01'),
+//         new Date('2019-12-31'),
+//         'abc'
+//     ),
+//     new Place(
+//         'p3',
+//         'The Foggy Palace',
+//         'Not your average city trip!',
+//         'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
+//         99.99,
+//         new Date('2019-01-01'),
+//         new Date('2019-12-31'),
+//         'abc'
+//     )
+// ]
+
 @Injectable({
     providedIn: 'root'
 })
 export class PlacesService {
     // tslint:disable-next-line: variable-name
-    private _places = new BehaviorSubject<Place[]>([
-        new Place(
-            'p1',
-            'Manhattan Mansion',
-            'In the heart of New York City.',
-            'https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200',
-            149.99,
-            new Date('2019-01-01'),
-            new Date('2019-12-31'),
-            'abc'
-        ),
-        new Place(
-            'p2',
-            "L'Amour Toujours",
-            'A romantic place in Paris!',
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Paris_Night.jpg/1024px-Paris_Night.jpg',
-            189.99,
-            new Date('2019-01-01'),
-            new Date('2019-12-31'),
-            'abc'
-        ),
-        new Place(
-            'p3',
-            'The Foggy Palace',
-            'Not your average city trip!',
-            'https://upload.wikimedia.org/wikipedia/commons/0/01/San_Francisco_with_two_bridges_and_the_fog.jpg',
-            99.99,
-            new Date('2019-01-01'),
-            new Date('2019-12-31'),
-            'abc'
-        )
-    ]);
+    private _places = new BehaviorSubject<Place[]>([]);
 
     get places() {
         return this._places.asObservable();
@@ -57,6 +70,39 @@ export class PlacesService {
                 return { ...places.find(p => p.id === id) };
             })
         );
+    }
+
+    fetchPlaces() {
+        return this.http
+            .get<{ [key: string]: PlaceData }>(
+                'https://ionic-angular-booking-ap-f1811.firebaseio.com/offered-places.json'
+            )
+            .pipe(
+                map(resData => {
+                    const places = [];
+                    for (const key in resData) {
+                        if (resData.hasOwnProperty(key)) {
+                            places.push(
+                                new Place(
+                                    key,
+                                    resData[key].title,
+                                    resData[key].description,
+                                    resData[key].imageUrl,
+                                    resData[key].price,
+                                    new Date(resData[key].availableFrom),
+                                    new Date(resData[key].availableTo),
+                                    resData[key].userId
+                                )
+                            );
+                        }
+                    }
+                    // return [];
+                    return places;
+                }),
+                tap(places => {
+                    this._places.next(places);
+                })
+            );
     }
 
     addPlace(
