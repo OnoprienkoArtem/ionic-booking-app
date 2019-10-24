cac95a9ce22284f4d03409e5a9df5f486e8b4264
OnoprienkoArtem <o.artem.e@gmail.com>
2019-10-24 17:34:39 +0300

    get image map

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -3,7 +3,9 @@ import { ModalController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
 import { environment } from '../../../../environments/environment';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
+import { PlaceLocation } from '../../../places/location.model';
+import { of } from 'rxjs';
 
 @Component({
     selector: 'app-location-picker',
@@ -23,13 +25,30 @@ export class LocationPickerComponent implements OnInit {
                     if (!modalData.data) {
                         return;
                     }
-                    this.getAddress(
-                        modalData.data.lat,
-                        modalData.data.lng
-                    ).subscribe(address => {
-                        console.log(address);
-                    });
+                    const pickedLocation: PlaceLocation = {
+                        lat: modalData.data.lat,
+                        lng: modalData.data.lng,
+                        address: null,
+                        staticMapImageUrl: null
+                    };
+                    this.getAddress(modalData.data.lat, modalData.data.lng)
+                        .pipe(
+                            switchMap(address => {
+                                pickedLocation.address = address;
+                                return of(
+                                    this.getMapImage(
+                                        pickedLocation.lat,
+                                        pickedLocation.lng,
+                                        14
+                                    )
+                                );
+                            })
+                        )
+                        .subscribe(staticMapImageUrl => {
+                            pickedLocation.staticMapImageUrl = staticMapImageUrl;
+                        });
                 });
+
                 modalEl.present();
             });
     }
@@ -51,5 +70,10 @@ export class LocationPickerComponent implements OnInit {
                     return geoData.results[0].formatted_address;
                 })
             );
+    }
+
+    private getMapImage(lat: number, lng: number, zoom: number) {
+        // tslint:disable-next-line: max-line-length
+        return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&maptype=roadmap&markers=color:red%7Clabel:PlaceS%7C${lat},${lng}&key=${environment.googleMapsAPIKey}`;
     }
 }
