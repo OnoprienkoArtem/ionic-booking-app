c966ae8e50f55cd4f1be57763953ba7cf7376975
OnoprienkoArtem <o.artem.e@gmail.com>
2019-10-24 13:29:46 +0300

    get address from pick on map

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
+import { environment } from '../../../../environments/environment';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-location-picker',
@@ -8,7 +11,7 @@ import { MapModalComponent } from '../../map-modal/map-modal.component';
     styleUrls: ['./location-picker.component.scss']
 })
 export class LocationPickerComponent implements OnInit {
-    constructor(private modalCtrl: ModalController) {}
+    constructor(private modalCtrl: ModalController, private http: HttpClient) {}
 
     ngOnInit() {}
 
@@ -17,9 +20,36 @@ export class LocationPickerComponent implements OnInit {
             .create({ component: MapModalComponent })
             .then(modalEl => {
                 modalEl.onDidDismiss().then(modalData => {
-                    console.log(modalData.data);
+                    if (!modalData.data) {
+                        return;
+                    }
+                    this.getAddress(
+                        modalData.data.lat,
+                        modalData.data.lng
+                    ).subscribe(address => {
+                        console.log(address);
+                    });
                 });
                 modalEl.present();
             });
+    }
+
+    private getAddress(lat: number, lng: number) {
+        return this.http
+            .get<any>(
+                `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${environment.googleMapsAPIKey}`
+            )
+            .pipe(
+                map(geoData => {
+                    if (
+                        !geoData ||
+                        !geoData.results ||
+                        geoData.results.length === 0
+                    ) {
+                        return null;
+                    }
+                    return geoData.results[0].formatted_address;
+                })
+            );
     }
 }
