93d1fa26318fcb1a45467a04c52fc80412bd8ac1
OnoprienkoArtem <o.artem.e@gmail.com>
2019-11-01 22:55:51 +0200

    fix mistake

--- src/app/places/offers/offer-item/offer-item.component.ts
+++ src/app/places/offers/offer-item/offer-item.component.ts
@@ -2,19 +2,14 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Place } from '../../place.model';
 
 @Component({
-  selector: 'app-offer-item',
-  templateUrl: './offer-item.component.html',
-  styleUrls: ['./offer-item.component.scss'],
+    selector: 'app-offer-item',
+    templateUrl: './offer-item.component.html',
+    styleUrls: ['./offer-item.component.scss']
 })
 export class OfferItemComponent implements OnInit {
+    @Input() place: Place;
 
-  @Input() place: Place;
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
-
-
+    ngOnInit() {}
 }
-˝
