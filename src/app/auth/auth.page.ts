3b3a1cf8860e203cf492e97cf3664ce07c947445
OnoprienkoArtem <o.artem.e@gmail.com>
2019-11-01 23:56:54 +0200

    work with auth signup

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -4,58 +4,53 @@ import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 
-
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss'],
+    selector: 'app-auth',
+    templateUrl: './auth.page.html',
+    styleUrls: ['./auth.page.scss']
 })
 export class AuthPage implements OnInit {
+    isLogin = true;
 
-  isLogin = true;
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private loadingCtrl: LoadingController
+    ) {}
 
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-    private loadingCtrl: LoadingController
-    ) { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
-
-  onLogin() {
-    this.authService.login();
-    this.loadingCtrl
-      .create({keyboardClose: true, message: 'Loggin in ...'})
-      .then(loadingEl => {
-        loadingEl.present();
-        setTimeout(() => {
-          loadingEl.dismiss();
-          this.router.navigateByUrl('/places/tabs/discover');
-        }, 1500);
-      });
-
-  }
-
-  onSubmit(form: NgForm) {
-    if (!form.valid) {
-      return;
+    onLogin() {
+        this.authService.login();
+        this.loadingCtrl
+            .create({ keyboardClose: true, message: 'Login in ...' })
+            .then(loadingEl => {
+                loadingEl.present();
+                setTimeout(() => {
+                    loadingEl.dismiss();
+                    this.router.navigateByUrl('/places/tabs/discover');
+                }, 1500);
+            });
     }
-    const email = form.value.email;
-    const password = form.value.password;
-    console.log(form);
-    console.log(email, password);
 
-    if (this.isLogin) {
-      // Send a request to login servers
-    } else {
-      // Send a request to signup servers
-      this.authService.signup(email, password);
+    onSubmit(form: NgForm) {
+        if (!form.valid) {
+            return;
+        }
+        const email = form.value.email;
+        const password = form.value.password;
+
+        if (this.isLogin) {
+            // Send a request to login servers
+        } else {
+            // Send a request to signup servers
+            this.authService.signup(email, password).subscribe(resData => {
+              console.log(resData);
+            });
+        }
     }
-  }
 
-  onSwitchAuthModel() {
-    this.isLogin = !this.isLogin;
-  }
-
+    onSwitchAuthModel() {
+        this.isLogin = !this.isLogin;
+    }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+
+interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    localId: string;
+    expiresIn: string;
+    registered?: boolean;
+}
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
     // tslint:disable-next-line: variable-name
-    private _userIsAuthenticated = true;
+    private _userIsAuthenticated = false;
     // tslint:disable-next-line: variable-name
-    private _userId = 'abc';
+    private _userId = null;
 
     get userIsAuthenticated() {
         return this._userIsAuthenticated;
@@ -21,7 +31,7 @@ export class AuthService {
     constructor(private http: HttpClient) {}
 
     signup(email: string, password: string) {
-        return this.http.post(
+        return this.http.post<AuthResponseData>(
             `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
             { email, password, returnSecureToken: true }
         );
