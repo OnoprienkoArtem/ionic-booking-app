f60b28082600397e467b7a6547165e3d57eeb0d3
OnoprienkoArtem <o.artem.e@gmail.com>
2019-11-02 21:01:03 +0200

    fix issues in booking service

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -41,43 +41,41 @@ export class BookingService {
         dateTo: Date
     ) {
         let generatedId: string;
-        const newBooking = new Booking(
-            Math.random().toString(),
-            placeId,
-            this.authService.userId,
-            placeTitle,
-            placeImage,
-            firstName,
-            lastName,
-            guestNumber,
-            dateFrom,
-            dateTo
+        let newBooking: Booking;
+        this.authService.userId.pipe(
+            take(1),
+            switchMap(userId => {
+                if (!userId) {
+                    throw new Error('No user id found!');
+                }
+                newBooking = new Booking(
+                    Math.random().toString(),
+                    placeId,
+                    userId,
+                    placeTitle,
+                    placeImage,
+                    firstName,
+                    lastName,
+                    guestNumber,
+                    dateFrom,
+                    dateTo
+                );
+
+                return this.http.post<{ name: string }>(
+                    'https://ionic-angular-booking-ap-f1811.firebaseio.com/bookings.json',
+                    { ...newBooking, id: null }
+                );
+            }),
+            switchMap(resData => {
+                generatedId = resData.name;
+                return this.bookings;
+            }),
+            take(1),
+            tap(bookings => {
+                newBooking.id = generatedId;
+                this._bookings.next(bookings.concat(newBooking));
+            })
         );
-
-        return this.http
-            .post<{ name: string }>(
-                'https://ionic-angular-booking-ap-f1811.firebaseio.com/bookings.json',
-                { ...newBooking, id: null }
-            )
-            .pipe(
-                switchMap(resData => {
-                    generatedId = resData.name;
-                    return this.bookings;
-                }),
-                take(1),
-                tap(bookings => {
-                    newBooking.id = generatedId;
-                    this._bookings.next(bookings.concat(newBooking));
-                })
-            );
-
-        // return this.bookings.pipe(
-        //     take(1),
-        //     delay(1000),
-        //     tap(bookings => {
-        //         this._bookings.next(bookings.concat(newBooking));
-        //     })
-        // );
     }
 
     cancelBooking(bookingId) {
