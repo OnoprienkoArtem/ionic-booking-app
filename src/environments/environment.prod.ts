388d50eb9da3887db22e912492b54afa45879c0b
OnoprienkoArtem <o.artem.e@gmail.com>
2019-11-13 23:08:48 +0200

    small fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  googleMapsAPIKey: 'AIzaSyCotJAi1wQ9oARZa_YP0yREfqMLkDDlP5Q',
-  firebaseAPIKey: 'AIzaSyCNLPlZJi5EhhC9JcUoRwlZDaS0uO98BfU'
+  googleMapsAPIKey: '',
+  firebaseAPIKey: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  googleMapsAPIKey: 'AIzaSyCotJAi1wQ9oARZa_YP0yREfqMLkDDlP5Q',
-  firebaseAPIKey: 'AIzaSyCNLPlZJi5EhhC9JcUoRwlZDaS0uO98BfU'
+  googleMapsAPIKey: '',
+  firebaseAPIKey: ''
 };
 
 /*
